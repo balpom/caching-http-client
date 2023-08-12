86abf222d7843a415ef954c45b51b8de2168b8ab
Mikhail (Balpom) <>
2023-08-12 16:59:47 +0300

    Initial commit.

---
+++ src/CachingHttpClient.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace CachingHttpClient;
+
+use Psr\Http\Client\ClientInterface;
+use Psr\SimpleCache\CacheInterface;
+use Balpom\HttpCacheTtl\TtlContainer;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class CachingHttpClient implements ClientInterface
+{
+    private ClientInterface $client;
+    private CacheInterface $cache;
+    private TtlContainer $times;
+
+    public function __construct(
+            ClientInterface $client,
+            CacheInterface $cache,
+            TtlContainer $times
+    )
+    {
+        $this->client = $client;
+        $this->cache = $cache;
+        $this->times = $times;
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        $key = $this->getKey($request);
+        if ($this->cache->has($key)) {
+            return $this->cache->get($key);
+        }
+
+        $response = $this->httpClient->sendRequest($request);
+        $code = $response->getStatusCode();
+        $ttl = $this->times->getTtl($code);
+        $this->cache->set($key, $response, $ttl);
+
+        return $response;
+    }
+
+    private function getKey(RequestInterface $request)
+    {
+        $method = $request->getMethod();
+        $uri = $request->getUri()->__toString();
+
+        return $method . '_' . $uri;
+    }
+
+}
